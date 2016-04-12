8a69b8ad259cab561d203e4669aef3d486a8cf85
Bruno da Luz <brunolabx@gmail.com>
2016-04-11 22:15:31 -0300

    altered device orientation and deployment target for 8.0

---
+++ iTetris/iTetrisTests/iTetrisTests.swift
@@ -0,0 +1,36 @@
+//
+//  iTetrisTests.swift
+//  iTetrisTests
+//
+//  Created by Bruno Da luz on 11/04/16.
+//  Copyright © 2016 Bruno v0id. All rights reserved.
+//
+
+import XCTest
+@testable import iTetris
+
+class iTetrisTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
